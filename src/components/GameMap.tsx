686187e295ba0212480c440bfea813d9bf8e667f
Qbao <lehoquocbao9@gmail.com>
2026-09-07 11:32:13 +0700

    Fix map tiles not loading: switch to CARTO tile provider (OSM deprecated shard subdomains unreliable)

--- src/components/GameMap.tsx
+++ src/components/GameMap.tsx
@@ -27,8 +27,9 @@ export function GameMap({ center, bases, currentPath, myAddress }: GameMapProps)
   return (
     <MapContainer center={[center.lat, center.lng]} zoom={16} style={{ height: "100%", width: "100%" }}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
       />
 
       {bases.map((base) => {
